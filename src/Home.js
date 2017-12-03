d6956fc259740a0542eefc6996d5bf50ac3736fc
Norman Steger <me@normansteger.com>
2017-12-03 20:35:08 +0100

    rename `App` references to `Home`

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+       <h1>Norman Steger</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
