8f7500dd949d9c5e2d5dad46dc828afe2e267949
Norman Steger <me@normansteger.com>
2018-03-03 22:11:40 +0100

    setup basic integration tests for existing components

--- src/Home.test.js
+++ src/Home.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './Home';
+import React from 'react'
+import { shallow } from 'enzyme'
+import Home from './Home'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Home />, div);
-});
+  const wrapper = shallow(<Home />)
+  expect(wrapper.find('.Home')).toHaveLength(1)
+})
