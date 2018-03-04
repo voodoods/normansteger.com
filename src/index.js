c98610a219db8b870e175efcd27e2d2279683686
Norman Steger <me@normansteger.com>
2018-03-04 21:23:49 +0100

    add react router and prop types

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Home from './home/Home'
-import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<Home />, document.getElementById('root'))
+import Header from './header/Header'
+import Home from './home/Home'
+import Skills from './skills/Skills'
+
+import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+ReactDOM.render(
+  <div className="wrapper">
+    <Header />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/skills" component={Skills} />
+      </Switch>
+    </BrowserRouter>
+  </div>,
+  document.getElementById('root')
+)
 registerServiceWorker()
