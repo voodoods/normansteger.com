af09ab8ce3ec488799c32e85569bd80ca93cd741
Norman Steger <me@normansteger.com>
2018-03-04 02:37:35 +0100

    add social contacts component

---
+++ src/social-contacts/SocialContacts.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const StyledSocialContacts = styled.ul`
+  list-style: none;
+
+  li {
+    display: inline-block;
+    margin-right: 5px;
+  }
+`
+
+export default StyledSocialContacts
