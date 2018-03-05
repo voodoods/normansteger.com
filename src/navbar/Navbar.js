d492d0c482889742738f63a134ba7ec757326353
Norman Steger <me@normansteger.com>
2018-03-05 23:29:42 +0100

    enable active status for navbar items

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import StyledNavbar from './Navbar.styled'
 
 class Navbar extends Component {
@@ -7,18 +8,28 @@ class Navbar extends Component {
       <StyledNavbar className="Navbar">
         <ul>
           <li>
-            <a href="/skills">My Professions</a>
+            <a href="/skills" className={this.props.path === '/skills' ? 'active' : 'inactive'}>
+              My Professions
+            </a>
           </li>
           <li>
-            <a href="/about-me">About me</a>
+            <a href="/about-me" className={this.props.path === '/about-me' ? 'active' : 'inactive'}>
+              About me
+            </a>
           </li>
           <li>
-            <a href="/hire-me">Hire me</a>
+            <a href="/hire-me" className={this.props.path === '/hire-me' ? 'active' : 'inactive'}>
+              Hire me
+            </a>
           </li>
         </ul>
       </StyledNavbar>
     )
   }
+}
+
+Navbar.propTypes = {
+  path: PropTypes.string,
 }
 
 export default Navbar
