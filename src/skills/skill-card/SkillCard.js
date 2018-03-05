c701a306b64763ba6b44a9b41ae657dddaca5635
Norman Steger <me@normansteger.com>
2018-03-05 22:51:26 +0100

    add centered mode to skillcard component

--- src/skills/skill-card/SkillCard.js
+++ src/skills/skill-card/SkillCard.js
@@ -5,10 +5,14 @@ import PropTypes from 'prop-types'
 class SkillCard extends Component {
   render() {
     return (
-      <StyledSkillCard className="SkillCard">
-        <img src={this.props.logo} alt={this.props.name} />
-        <h3>{this.props.name}</h3>
-        <p>{this.props.description}</p>
+      <StyledSkillCard className="SkillCard" centered={this.props.centered}>
+        <div className="skill-logo">
+          <img src={this.props.logo} alt={this.props.name} />
+        </div>
+        <div className="skill-story">
+          <h3>{this.props.name}</h3>
+          <p>{this.props.description}</p>
+        </div>
       </StyledSkillCard>
     )
   }
@@ -18,6 +22,7 @@ SkillCard.propTypes = {
   logo: PropTypes.string,
   name: PropTypes.string,
   description: PropTypes.string,
+  centered: PropTypes.booelean,
 }
 
 export default SkillCard
